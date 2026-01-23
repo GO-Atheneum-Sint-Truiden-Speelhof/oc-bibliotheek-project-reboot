2482f5ec8499abe4109362551f73d6a6a3adaff4
Noxchi-umr <umar.abdoulvakhidov@atheneumsinttruiden.be>
2026-01-23 09:32:57 +0100

    index

--- OC BIB/index.php
+++ OC BIB/index.php
@@ -12,70 +12,29 @@
 </header>
 
 <body>
-	<div class="jumbotron" class="display-4">
+	
+	<div class="card-header"><h3 class="text-center fond-weight-light my-4">Boek toevoegen></h3></div>
 
 	<form action="" method="post">
-
-		<div class="col tegel" class="form-container">
-			<div class="col">
-				<label for="titel">Titel</label>
-				<p><input type="text" name="titel" size="30"></p>
-			</div>
-
-			<div class="col">
-				<label for="coverfoto">Cover foto</label>
-				<p><input type="file" id="coverfoto" name="coverfoto" accept="image/png, image/jpeg, image/webp"></p>
-			</div>
-		</div>
-
-		<div class="col tegel" class="form-container">
-			<div class="col">
-				<label for="auteur">Auteur</label>
-				<p><input type="text" name="auteur" size="30"></p>
-			</div>
-
-			<div class="col">
-				<label for="isbn">ISBN</label>
-				<p><input type="text" name="isbn" size="30"></p>
-			</div>
-		</div>
-
-		<div class="col tegel" class="form-container">
-			<div class="col">
-				<label for="sumary">Summary</label>
-				<p><input type="text" name="sumary" size="60"></p>
-			</div>
-
-			<div class="col">
-				<label for="genre">Genre</label>
-				<p>
-					<select>
-						<option value="waarde1">Psychologie</option>
-						<option value="waarde2" selected>Action</option>
-						<option value="waarde3">Horror</option>
-					</select>
-				</p>
-
-				<div class="col-md">
-					<button type="button" onclick="Toevoegen()">Toevoegen</button>
+		<div class="row mb-3">
+			<div class="col-md-6">
+				<div class="form-floating mb-3 mb-md-0">
+					<input class="form control" name="isbn" type="text" placeholder="Enter ISBN" />
+					<label for="isbn">ISBN-nummer</label>
 				</div>
-
 			</div>
-		</div>
-	</div>
-</body>
-
-</html>
-
-<div class="card-header"><h3 class="text-center fond-weight-light my-4">Boek toevoegen></h3></div>
-
-<form action="" method="post">
-	<div class="row mb-3">
-		<div class="col-md-6">
-			<div class="form-floating mb-3 mb-md-0">
-				<input class="form control" name="isbn" type="text" placeholder="Enter ISBN" />
-				<label for="isbn">ISBN-nummer</label>
+			<div class="col-md-6">
+				<div class="form-floating mb-3 mb-md-0">
+					<input class="form control" name="titel" type="text" placeholder="Enter titel" />
+					<label for="titel">Titel</label>
+				</div>
 			</div>
 		</div>
 		
-</form>
+		<div class="col-md">
+			<button type="button" onclick="Toevoegen()">Toevoegen</button>
+		</div>
+	</form>
+</body>
+
+</html>
