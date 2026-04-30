053ca7a65afd0ecae48647138a091a1c218d9a35
AnAv3rageGam3r <stan.meirens@atheneumsinttruiden.be>
2026-04-30 14:18:13 +0200

    styles/opmaak.css in opmaak/opmaak.css gestoken

--- OC BIB/includes/nav.php
+++ OC BIB/includes/nav.php
@@ -4,5 +4,6 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="Books.php">Boeken</a></li>
         <li><a href="login.php">Log in</a></li>
+        <li><a href="QR Code/qr_generate.php">qr-code genereren</a></li>
     </ul>
 </nav>
