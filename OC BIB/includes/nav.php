e9693b6ef28e9e80eebb5bac449d811b229a58bb
AnAv3rageGam3r <stan.meirens@atheneumsinttruiden.be>
2026-04-30 14:25:17 +0200

    styles/opmaak.css naar veranderd naar de opmaak.indexOpmaak zodat ze allemaal in 1 map steken

--- OC BIB/includes/nav.php
+++ OC BIB/includes/nav.php
@@ -2,7 +2,7 @@
 <nav>
     <ul class="nav-list">
         <li><a href="index.php">Home</a></li>
-        <li><a href="Books.php">Boeken</a></li>
+        <li><a href="boekoverzicht.php">Boeken</a></li>
         <li><a href="login.php">Log in</a></li>
         <li><a href="QR Code/qr_generate.php">qr-code genereren</a></li>
     </ul>
