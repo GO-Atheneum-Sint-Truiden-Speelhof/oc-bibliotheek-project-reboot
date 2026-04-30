d8fda2eab53bdf8c81c83c9979889bc75612e056
AnAv3rageGam3r <stan.meirens@atheneumsinttruiden.be>
2026-04-30 13:01:50 +0200

    Add functions to retrieve all ISBNs and book information by ISBN

--- OC BIB/server/functions.php
+++ OC BIB/server/functions.php
@@ -45,5 +45,38 @@
         return password_hash($row[0], PASSWORD_DEFAULT);
         //returns the hashed password for the given username, or an error message if the user is not found
     }
-    
+    function getIsbnAllBooks(){
+        $db = connectDB();
+        $qry = "SELECT `ISBN` FROM `book`";
+        $stmt = $db->prepare($qry);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows === 0) {
+            $db->close();
+            die("Er zijn geen boeken in de database");
+        }
+        $isbns = [];
+        while ($row = $result->fetch_row()) {
+            $isbns[] = $row[0];
+        }
+        $db->close();
+        return $isbns;
+        // returns an array of all ISBNs in the book database, or an error message if there are no books in the database
+    }
+    function getInfoBook($ISBN){
+        $db = connectDB();
+        $qry = "SELECT `Title`, `Author`, `Summary`, `ISBN`, `RentedOut`, `Cover`, `QR`, `Genre`, `Pages`, `Age` FROM `book` WHERE `ISBN` = ?";
+        $stmt = $db->prepare($qry);
+        $stmt->bind_param("s", $ISBN);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows === 0) {
+            $db->close();
+            die("Boek niet gevonden");
+        }
+        $row = $result->fetch_assoc();
+        $db->close();
+        return $row;
+        // returns an associative array with all the information of the book with the given ISBN, or an error message if the book is not found
+    }    
 ?>
